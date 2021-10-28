fa1031d381ec7a7af7e65507af8fc236dc1e69d3
Michalkonik99 <michalkonik13@gmail.com>
2021-10-28 03:22:48 +0200

    v0.1 + build

---
+++ cmake-build-debug/project_1/matrixApp/CMakeFiles/projectMatrix.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projectMatrix.dir/main.cpp.o"
+  "projectMatrix"
+  "projectMatrix.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/projectMatrix.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
