9b5a855a9cd19e1d4be20db6b0910e5907789034
Michalkonik99 <michalkonik13@gmail.com>
2021-11-01 18:20:11 +0100

    homwework

---
+++ zadania_lab04/cmake-build-debug/CMakeFiles/homework_lab04.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/homework_lab04.dir/lib/zadanie_01.cpp.o"
+  "CMakeFiles/homework_lab04.dir/src/main.cpp.o"
+  "homework_lab04"
+  "homework_lab04.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/homework_lab04.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
