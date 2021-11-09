bdf4255a7c2cbc33c3db2608d4ad55db4aa89031
Michalkonik99 <michalkonik13@gmail.com>
2021-11-09 10:42:40 +0100

    lab_05

--- zadania_lab05/include/Vehicle.h
+++ zadania_lab05/include/Vehicle.h
@@ -13,9 +13,15 @@ private:
     string brand;
     string type;
 
+    static double newest_software_version;
+    double installed_software_version = 3.0;
+
 public:
     Vehicle(int, string, int, char*, string, string);
     ~Vehicle();
     void print_data();
+    void print_software_version();
+    void update_software();
+    static void release_new_software_version(double released_version);
 };
 #endif //E1NOCOPYINGCONSTRUCTOR_VEHICLE_H
--- zadania_lab05/src/Vehicle.cpp
+++ zadania_lab05/src/Vehicle.cpp
@@ -1,11 +1,14 @@
 #include "Vehicle.h"
+#include <string.h>
+
+double Vehicle::newest_software_version = 3.12;
 
 Vehicle::Vehicle(int Register_Number, string Name, int Number_Of_Seats, char* Owner, string Brand, string Type){
     register_number = Register_Number;
     name = Name;
     number_of_seats = Number_Of_Seats;
     owner = new char[20];
-    owner = Owner;
+    strcpy(owner, Owner);
     brand = Brand;
     type = Type;
 }
@@ -21,4 +24,17 @@ void Vehicle::print_data(){
     cout << owner << endl;
     cout << brand << endl;
     cout << type << endl;
+}
+
+void Vehicle::print_software_version() {
+    cout << "Zainstalowana wersja oprogramowania: " << installed_software_version << endl;
+}
+
+void Vehicle::update_software() {
+    installed_software_version = newest_software_version;
+}
+
+void Vehicle::release_new_software_version(double released_version) {
+    newest_software_version = released_version;
+    cout << "Najnowsze oprogramowanie: " << newest_software_version << endl;
 }
