5058518d5b7f10fe4e1404178247a3f0ef122ea4
Michalkonik99 <michalkonik13@gmail.com>
2021-11-13 21:44:24 +0100

    lab_05

---
+++ zadania_lab05/src/main.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include "Point.h"
+#include "Vehicle.h"
+
+using namespace std;
+
+class TestClass {
+private:
+    const int t = 0;
+
+public:
+    TestClass(const int t) : t(t) {
+    }
+};
+
+int main() {
+//    int cVal = 314;
+//    int *const p = &cVal;
+//
+//    *p = 628;
+//
+//    int cVal2 = 666;
+//    p = &cVal2;
+
+    Vehicle vehicle = Vehicle(2, "nazwa", 4, "imie nazwisko", "marka", "typ");
+    vehicle.print_data();
+    vehicle.print_software_version();
+    vehicle.update_software();
+    vehicle.print_software_version();
+    Vehicle::release_new_software_version(4.1);
+
+//    {
+//        Point p1("P1"), p2(5, 5, "P2");
+//
+//        cout << "Initial values" << endl;
+//        p1.printData();
+//        p2.printData();
+//
+//        cout << endl << "Create copy of point" << endl;
+//        Point p1c = p1, p2c = p2;
+//        //Update copied points names
+//        p1c.setName("P1c");
+//        p2c.setName("P2c");
+//
+//        cout << "Values after copying" << endl;
+//        p1.printData();
+//        p2.printData();
+//        p1c.printData();
+//        p2c.printData();
+//
+//        cout << endl << "Update P1 and P2 x and y (increase 10)" << endl;
+//        p1.setX(10);
+//        p1.setY(10);
+//        p2.setX(15);
+//        p2.setY(15);
+//
+//        cout << "Values after update values" << endl;
+//        p1.printData();
+//        p2.printData();
+//        p1c.printData();
+//        p2c.printData();
+//    }
+//
+//
+//    cout << "End program" << endl;
+
+
+    return 0;
+}
