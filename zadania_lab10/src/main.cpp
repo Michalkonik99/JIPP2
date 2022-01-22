ba584a2bc9b8809f8ff367b9ccdee475afc17b41
Michalkonik99 <michalkonik13@gmail.com>
2022-01-22 14:48:39 +0100

    lab_10

---
+++ zadania_lab10/src/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+//    std::ofstream outfile ("input.txt");
+//    ifstream input("input.txt");
+//    if(input.is_open()) {
+//        string word;
+//        while(!input.eof()) {
+//            input >> word;
+//            cout << word << " ";
+//        }
+//        input.close();
+//    } else {
+//        cout << "Error on opening file" << endl;
+//        exit(1);
+//    }
+
+
+
+    return 0;
+}
